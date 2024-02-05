0a5b2c6a84b9ecb82b9bdb599e1be2d7a89857da
Br0deNN <theo.broden@gmail.com>
2024-02-05 19:41:10 +0100

    Add project files.

---
+++ List uppgift 4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] myString = { "hej", "nej", "ja" };       
+        Console.WriteLine(FinnsTecknetIAllaSträngar(myString,'e')); //e blir false och h blir true
+
+
+    }
+
+    static bool FinnsTecknetIAllaSträngar(string[] myString, char myChar)
+    {
+        bool finnsIalla = true;
+
+        for (int i = 0; i < myString.Length; i++)
+        {
+            bool xstring = false;
+            for (int j = 0; j < myString[i].Length; j++)
+            {
+                if (myString[i][j] == myChar)
+                {
+                    xstring = true;
+                    break;
+                }
+            }
+
+            if (xstring == false)
+            {
+                finnsIalla = false;
+                break;
+            }
+        }
+        return finnsIalla;
+
+        
+    }
+}
